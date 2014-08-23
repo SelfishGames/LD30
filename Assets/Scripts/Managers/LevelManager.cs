84f612f1c07091cf35197f168ff2ada8030db61f
SW34TY <andrewashton34@gmail.com>
2014-08-23 16:55:12 +0100

    vxh

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,83 +1,62 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : MonoBehaviour 
 {
-    public GameManager gameManager;
-
-    public Transform player;
-    public Transform nearWall;
-
     //True is day, false is night
     [HideInInspector]
     public bool worldState = true;
-    [HideInInspector]
-    public int currentLevel = 0;
-    public List<GameObject> levels = new List<GameObject>();
 
     //Camera vars
     private float targetRotation;
     private float rotateAmount = 55f;
 
-    private Color tempWallColour;
-
 	void Start () 
-        {
-            tempWallColour = nearWall.renderer.material.color;
+    {
+	
 	}
 
     #region Update
     void Update()
     {
-        //Changes worldStates
+        //Changes worldStates 
         if (Input.GetKeyDown(KeyCode.C))
         {
             if (worldState)
             {
                 worldState = false;
+                targetRotation = 0f;
             }
             else
             {
                 worldState = true;
+                targetRotation = 90f;
             }
         }
 
-        Debug.Log(worldState);
-        switch (worldState)
+        switch(worldState)
         {
-            //Day
+                //Day
             case true:
                 {
-                    if (!gameManager.cameraManager.ChangeView(2, Vector3.up))
-                        break;
-
-                    nearWall.renderer.material.color = Color.Lerp(
-                        nearWall.renderer.material.color, tempWallColour, Time.deltaTime);
-
+                    if (targetRotation > 0)
+                    {
+                        Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, rotateAmount * Time.deltaTime);
+                        targetRotation -= rotateAmount * Time.deltaTime;
+                    }
                     return;
                 }
-            //Night
+                //Night
             case false:
                 {
-                    if (!gameManager.cameraManager.ChangeView(1, Vector3.down))
-                        break;
-
-                    nearWall.renderer.material.color = Color.Lerp(
-                        nearWall.renderer.material.color, Color.clear, Time.deltaTime);
-
+                    if (targetRotation < 90)
+                    {
+                        Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, -rotateAmount * Time.deltaTime);
+                        targetRotation += rotateAmount * Time.deltaTime;
+                    }
                     return;
                 }
+        }
+    }
     #endregion
-        }
-    }
-
-    public void ArrangeObstacles()
-    {
-        foreach(GameObject go in levels)
-        {
-            go.SetActive(false);
-            levels[currentLevel].SetActive(true);
-
-        }
-    }
 }
