a9cd0cf090fe8d559812f7a958f005f534c13c11
Rob <az5rob@gmail.com>
2014-08-23 15:48:42 +0100

    implemented enemy.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    public CameraManager cameraManager;
     public LevelManager levelManager;
+    public PlayerManager playerManager;
 
-    void Start()
-    {
-
-    }
 
     void Update()
     {
-
+    	if(cameraManager.transform.GetChild(0).gameObject.activeSelf && Input.GetKeyDown("return"))
+    	{
+    		cameraManager.transform.GetChild(0).gameObject.SetActive(false);
+    	}
     }
 }
--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class LevelManager : MonoBehaviour 
+public class LevelManager : MonoBehaviour
 {
     //True is day, false is night
     [HideInInspector]
     public bool worldState = true;
+    [HideInInspector]
+    public int currentLevel = 0;
+    public List<GameObject> levels = new List<GameObject>();
 
     //Camera vars
     private float targetRotation;
     private float rotateAmount = 55f;
 
-	void Start () 
-    {
-	
-	}
+    void Start ()
+    {   
+
+    }
 
     #region Update
     void Update()
     {
-        //Changes worldStates 
+        //Changes worldStates
         if (Input.GetKeyDown(KeyCode.C))
         {
             if (worldState)
@@ -34,29 +37,40 @@ public class LevelManager : MonoBehaviour
             }
         }
 
-        switch(worldState)
+        switch (worldState)
         {
-                //Day
-            case true:
-                {
-                    if (targetRotation > 0)
-                    {
-                        Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, rotateAmount * Time.deltaTime);
-                        targetRotation -= rotateAmount * Time.deltaTime;
-                    }
-                    return;
-                }
-                //Night
-            case false:
-                {
-                    if (targetRotation < 90)
-                    {
-                        Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, -rotateAmount * Time.deltaTime);
-                        targetRotation += rotateAmount * Time.deltaTime;
-                    }
-                    return;
-                }
+        //Day
+        case true:
+        {
+            if (targetRotation > 0)
+            {
+                Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, rotateAmount * Time.deltaTime);
+                targetRotation -= rotateAmount * Time.deltaTime;
+            }
+            return;
+        }
+        //Night
+        case false:
+        {
+            if (targetRotation < 90)
+            {
+                Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, -rotateAmount * Time.deltaTime);
+                targetRotation += rotateAmount * Time.deltaTime;
+            }
+            return;
+        }
+
+        }
+
+        #endregion
+    }
+
+    public void ArrangeObstacles()
+    {
+        foreach(GameObject go in levels)
+        {
+            go.SetActive(false);
+            levels[currentLevel].SetActive(true);
         }
     }
-    #endregion
 }
